3389079d8244709883b85c1c386205f41282cd75
AvaLikesBread <127442971+YourAva@users.noreply.github.com>
2024-04-06 17:38:24 +0100

    Added Button Support

--- src/makeWindow.c
+++ src/makeWindow.c
@@ -1,15 +1,24 @@
 #include "windowLogic.h"
+#include <stdio.h>
+
+int genErrorMsg() {
+    MessageBox(NULL, "Window Loader encountered an error.", "Error!", MB_ICONEXCLAMATION | MB_OK);
+    return 0;
+}
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch(msg)
     {
+        case WM_COMMAND:
+            MessageBox(NULL, "Sike!", "Evil Window", MB_ICONEXCLAMATION | MB_OK);
+            PostQuitMessage(0);
         case WM_CLOSE:
             DestroyWindow(hwnd);
-        break;
+            break;
         case WM_DESTROY:
             PostQuitMessage(0);
-        break;
+            break;
         default:
             return DefWindowProc(hwnd, msg, wParam, lParam);
     }
@@ -43,8 +52,8 @@ int WINAPI makeWindow(char *title, int x_scale, int y_scale, int backgroundColor
 
     if(!RegisterClassEx(&wc)) // If we can't register a window class for subsequent use in calls, return an error box.
     {
-        MessageBox(NULL, "We shit the bed! (Ur window failed bruh)", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
+        genErrorMsg();
+        return -1;
     }
 
     hwnd = CreateWindowEx(                             // --- If you're looking at this, I highly recommend looking at all the paramaters that can go with it.
@@ -56,11 +65,26 @@ int WINAPI makeWindow(char *title, int x_scale, int y_scale, int backgroundColor
         NULL, NULL, hInstance, NULL);                  // Parent window handle, menu handle, application instance handle, pointer to window creation data.
     if(hwnd == NULL)
     {
-        MessageBox(NULL, "We shit the bed! (Ur window failed bruh)", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
+        MessageBox(NULL, "Window failed to initialise.", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return -1;
     }
 
-    ShowWindow(hwnd, SW_SHOWNORMAL); // created window data, if the window can start as minimised, fullscreened etc... This is fucking stupid. We're putting it under SW_SHOWNORMAL
+    HWND hwndButton = CreateWindow( 
+        "BUTTON",  // Predefined class; Unicode assumed 
+        "Press for cookies.",      // Button text 
+        WS_VISIBLE | WS_CHILD, // Styles 
+        (x_scale / 2) - 100, 10, 200, 50,  // x y w h -- The calculation at the start divides the window width by 2 to get half and then minuses half of the button width to center it.
+        hwnd,       // Parent window
+        NULL,       // No menu.
+        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), 
+        NULL);      // Pointer not needed.
+    if(hwndButton == NULL)
+    {
+        MessageBox(NULL, "Button failed to initialise.", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return -1;
+    }
+
+    ShowWindow(hwnd, SW_SHOWNORMAL); // created window data, if the window can start as minimised, fullscreened etc... This is stupid. We're putting it under SW_SHOWNORMAL
     UpdateWindow(hwnd);
 
     while(GetMessage(&Msg, NULL, 0, 0) > 0)
@@ -70,9 +94,4 @@ int WINAPI makeWindow(char *title, int x_scale, int y_scale, int backgroundColor
         DispatchMessage(&Msg); // Send message to the window that the data was sent to.
     }
     return Msg.wParam;
-}
-
-int callError(){
-    MessageBox(NULL, "We shit the bed! (Ur window failed bruh)", "Error!", MB_ICONEXCLAMATION | MB_OK);
-    return 0;
 }
